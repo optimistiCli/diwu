aa84317aaf4906ec24d23583b7dbe81b46a215af
Ish Yet13 <yet13@ema.il>
2025-10-18 12:25:59 +0200

    Improved prerequisitrs checker

--- prereq.sh
+++ prereq.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 /bin/bash -c '
     set -e
-    uname -s | grep -i linux
+    echo -n "Checking OS: " >&2
+    uname -s \
+        | grep -i linux >&2
     for U in bash mktemp find sed; do
+        echo -n "Checking $U command: " >&2
+        which $U >/dev/null
         $U --version \
         | head -n 1 \
-        | grep GNU
+        | grep -e GNU -e coreutils >&2
     done
-    id -Gn | grep docker
-    stat -c %G /var/run/docker.sock | grep docker
+    echo -n "Checking current user groups: " >&2
+    id -Gn \
+        | tr -s "[[:space:]]" "\n" \
+        | grep docker >&2
+    echo -n "Checking docker.sock: " >&2
+    stat -c %G /var/run/docker.sock \
+        | grep docker >&2
     echo "Looking good"
-' | grep "Looking good" \
-|| echo "Something is off"
+' \
+    | grep "Looking good" \
+    || echo "Something is off"
